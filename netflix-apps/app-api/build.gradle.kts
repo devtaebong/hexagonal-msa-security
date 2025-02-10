d78cfd9225b81a522b6d89113c4c37b5f9b030d2
김태현(TaeHyeon, Kim) <thk98k@gmail.com>
2025-02-10 23:46:54 +0900

    chore: spring security configuration

--- netflix-apps/app-api/build.gradle.kts
+++ netflix-apps/app-api/build.gradle.kts
@@ -3,4 +3,17 @@ dependencies {
     runtimeOnly(project(":netflix-core:core-service"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.springframework.security:spring-security-oauth2-client")
+    implementation("org.springframework.data:spring-data-commons")
+
+    val appMainClassName = "com.study.netflix.NetflixApiApplication"
+    tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+        mainClass.set(appMainClassName)
+        archiveClassifier.set("boot")
+    }
 }
